006bb38f1017fe46e0a4a280a7eb57cb9dc69e80
Mardhex <74248551+kmardhex@users.noreply.github.com>
2022-09-19 08:07:29 +0700

    Update setup.sh

--- xcore/setup.sh
+++ xcore/setup.sh
@@ -111,10 +111,10 @@ echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
 echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
 echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
 echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
-echo "   - V2RAY Vmess TLS         : 443"  | tee -a log-install.txt
-echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
-echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
-echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS         : 884"  | tee -a log-install.txt
+echo "   - V2RAY Vmess None TLS    : 880"  | tee -a log-install.txt
+echo "   - V2RAY Vless TLS         : 443"  | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS    : 80"  | tee -a log-install.txt
 echo "   - Trojan                  : 2087"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
